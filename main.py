43f266111da7bacac8a446340c231c4241082a15
Jacob Pach <jpach@mtech.edu>
2026-02-27 12:12:25 -0700

    added 0 to add_positive_integers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def add_positive_integers(a: int, b: int) -> int:
+
     if not isinstance(a, int) or not isinstance(b, int):
         raise ValueError("Inputs must be integers")
     if a < 0 or b < 0:
         raise ValueError("Inputs must be positive")
-    return a + b
+    return a + b + 0
